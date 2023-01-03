e8e4007ee6135a0cd969db6d19d937e9bb708186
ismailkisla <121893468+ismailkisla@users.noreply.github.com>
2023-01-03 21:34:23 +0300

    Update cloudflare_yedek.sh

--- cloudflare_yedek.sh
+++ cloudflare_yedek.sh
@@ -6,7 +6,6 @@ APIKEY=""
 
 mkdir -p zones
 curl -s -X GET "https://api.cloudflare.com/client/v4/zones??status=active&account.id=${ACCOUNTID}&page=1&per_page=150" -H  "X-Auth-Key:${APIKEY}" -H "X-Auth-Email:${ACCOUNTNAME}" -H "Content-Type:application/json" | jq -r '.result[] | .id' | while read -r line; do
-#curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${line}" -H "X-Auth-Email:${ACCOUNTNAME}" -H "X-Auth-Key:${APIKEY}" -H "Content-Type:application/json"
 
 domainname=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${line}" -H "X-Auth-Email:${ACCOUNTNAME}" -H "X-Auth-Key:${APIKEY}" -H "Content-Type:application/json" | jq -r '.result .name')
 echo  ${domainname}
